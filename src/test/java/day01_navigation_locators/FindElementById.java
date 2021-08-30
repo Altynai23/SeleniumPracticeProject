c54e605c40ea79d007f212ff4a95ab7040181354
Altynai23 <salyb1987@gmail.com>
2021-08-30 15:55:20 -0400

    Day01 : FindElementById for search button and click or submit

---
+++ src/test/java/day01_navigation_locators/FindElementById.java
@@ -0,0 +1,35 @@
+package day01_navigation_locators;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class FindElementById {
+
+    public static void main(String[] args) {
+
+        // open chrome and navigate to yahoo
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://www.yahoo.com/");
+
+        //Locate search box and enter selenium
+        WebElement searchBox = driver.findElement(By.id("ybar-sbq"));
+        // enter text inside inputbox element using sendKey("your text")
+        searchBox.sendKeys("Selenium");
+        // Locate the search button and click (or submit)
+        WebElement searchBtn = driver.findElement(By.id("ybar-sbq"));
+        searchBtn.click();
+
+        
+
+        // quit browser
+        driver.quit();
+
+    }
+
+
+}
