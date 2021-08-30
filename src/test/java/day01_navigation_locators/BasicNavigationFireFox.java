35613117f7d5acf60d41d118d40f48b413918c15
Altynai23 <salyb1987@gmail.com>
2021-08-30 14:35:54 -0400

    Day01 : firefox browser navigation complete, quited browser after

--- src/test/java/day01_navigation_locators/BasicNavigationFireFox.java
+++ src/test/java/day01_navigation_locators/BasicNavigationFireFox.java
@@ -1,8 +1,26 @@
 package day01_navigation_locators;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
 public class BasicNavigationFireFox {
 
     public static void main(String[] args) {
-        
+
+        // 1. sut up Firefox driver
+        WebDriverManager.firefoxdriver().setup();
+        // 2. Create Webdriver instance using FireFoxDriver object
+        WebDriver driver = new FirefoxDriver();
+        // 3. navigate to "https://cybertekschool.com"
+        driver.get("https://cybertekschool.com");
+
+
+        System.out.println("About the quit");
+        // quit the browser
+        driver.quit();
+
+
+
     }
 }
