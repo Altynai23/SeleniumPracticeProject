a01871685f0e73ff85ef9a87fe282ce660aaecf7
Altynai23 <salyb1987@gmail.com>
2021-09-13 16:33:57 -0400

    Day 05 : Yahoo Search Result Page Title Test

--- src/test/java/com/cybertek/tests/day05_css_xpatch_junit5/YahooSearchPageTest.java
+++ src/test/java/com/cybertek/tests/day05_css_xpatch_junit5/YahooSearchPageTest.java
@@ -2,10 +2,14 @@ package com.cybertek.tests.day05_css_xpatch_junit5;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class YahooSearchPageTest {
 
@@ -17,7 +21,7 @@ public class YahooSearchPageTest {
     //        the title should be "Yahoo Search - Web Search"
 
     @Test
-    public void testYahooSearchResultPageTitle(){
+    public void testYahooSearchHomePageTitle(){
 
         WebDriverManager.chromedriver().setup();
         WebDriver driver = new ChromeDriver();
@@ -40,6 +44,34 @@ public class YahooSearchPageTest {
     // test navigate to yahoo page
     // and search for Selenium
     // the page title should start with selenium
+
+    @Test
+    public void testYahooSearchResultPageTitle(){
+
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://search.yahoo.com/");
+
+        // identify search box and enter selenium , and hit Enter key on keyboard
+        WebElement searchBox = driver.findElement(By.xpath("//input[@name='p']"));
+        // we can simulate keystroke usinh Keys.SELECT_ONE_OF_THE_OPTION
+        // In this case we are typing selenium and hitter enter
+        searchBox.sendKeys("Selenium" + Keys.ENTER);
+
+//        String expectedTitleStartWith = "Selenium" ;
+        String actualTitle = driver.getTitle() ;
+
+        // quit the browser
+        driver.quit();
+        // assert the title starts with Selenium
+        assertTrue( actualTitle.startsWith("Selenium")  );
+
+
+
+    }
+
+
 
 
 }
