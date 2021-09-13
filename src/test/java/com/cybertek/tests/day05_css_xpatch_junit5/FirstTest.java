df203833b315f9d18e9b587d4b6251fd0744f614
Altynai23 <salyb1987@gmail.com>
2021-09-13 15:40:36 -0400

    Day 05 : First Junit Test with 2 Test methods

--- src/test/java/com/cybertek/tests/day05_css_xpatch_junit5/FirstTest.java
+++ src/test/java/com/cybertek/tests/day05_css_xpatch_junit5/FirstTest.java
@@ -3,8 +3,20 @@ package com.cybertek.tests.day05_css_xpatch_junit5;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-public class FirstTest {
+/**
+ * Test class name should be sth like this
+ * // SomethingTest.java
+ * // TestSomething.java
+ * // SomethingShouldDoThatTest.java
+ *
+ * //Test method  :  testAddition()
+ *                      additionTest()
+ *
+ */
 
+public class FirstTest {
+    // Test methods is annotated with @Test annotation
+    //It's a void method with no param
     @Test
     public void testAddition(){
 
@@ -12,6 +24,20 @@ public class FirstTest {
         int num2 = 7 ;
         int expectedResult = 17 ;
         Assertions.assertEquals(expectedResult , num1+num2);
+
+    }
+
+    @Test
+    public void testString(){
+
+        String msg = "I love Selenium";
+        // assert this msg equal "I love Selenium"
+        // assertEquals method accept 2 parameters
+        Assertions.assertEquals("I love Selenium", msg);
+
+        // assert this msg starts with "I love"
+        // assertTrue method accept 1 parameters as boolean
+        Assertions.assertTrue( msg.startsWith("I love")  );
 
     }
 
