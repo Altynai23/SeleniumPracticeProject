f547b897862bd1a05f60cd696fef615ddf94e010
Altynai23 <salyb1987@gmail.com>
2021-09-14 15:00:06 -0400

    Day 06 : Added Implicit wait to wait maximum 10 seconds

--- src/test/java/com/cybertek/utility/TestBase.java
+++ src/test/java/com/cybertek/utility/TestBase.java
@@ -1,10 +1,11 @@
 package com.cybertek.utility;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.concurrent.TimeUnit;
+
 
 /**
  * This class is meant to be super class
@@ -21,6 +22,7 @@ public abstract class TestBase {
 //        driver = new ChromeDriver();
 //        driver.manage().window().maximize();
         driver = WebDriverFactory.getDriver("chrome");
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS) ;
     }
 
     @AfterEach
