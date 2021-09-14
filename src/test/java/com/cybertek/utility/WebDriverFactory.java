5a1f61737d003ab87916cce9d4c98356454e811c
Altynai23 <salyb1987@gmail.com>
2021-09-14 13:12:42 -0400

    Day 06 : Created WebDriverFactory class to get any WebDriver object f…

---
+++ src/test/java/com/cybertek/utility/WebDriverFactory.java
@@ -0,0 +1,46 @@
+package com.cybertek.utility;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+/**
+ * We need a utility class ith method
+ * to get WebDriver object with all the settings needed
+ * by passing browserName
+ *
+ * WebDriverFactory.get("chrome") ==>> WebDriver object with Chrome Driver
+ * WebDriverFactory.get("firefox") ==>> WebDriver object with FireFox Driver
+ *
+ */
+
+public class WebDriverFactory {
+
+    public static WebDriver get(String browserName){
+
+        WebDriver driver;
+
+        switch (browserName){
+            case "chrome":
+                WebDriverManager.chromedriver().setup();
+                driver = new ChromeDriver();
+                break;
+            case "firefox" :
+                WebDriverManager.firefoxdriver().setup();
+                driver = new FirefoxDriver();
+                break;
+            // other browsers omitted
+            default:
+                driver = null ;
+                System.out.println("UNKNOWN BROWSER TYPE!!! " + browserName);
+        }
+
+        driver.manage().window().maximize();
+
+        return driver ;
+    }
+
+
+
+}
