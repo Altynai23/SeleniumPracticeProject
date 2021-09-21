09f1c1e01ef855917ad1e48d5e668477ea0883e7
Altynai23 <salyb1987@gmail.com>
2021-09-21 15:49:48 -0400

    Day 09 : using Utilities we created

--- src/test/java/com/cybertek/utility/WebOrderUtility.java
+++ src/test/java/com/cybertek/utility/WebOrderUtility.java
@@ -1,12 +1,14 @@
 package com.cybertek.utility;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 // THIS AIN'T NO TEST CLASS SO WE CAN NOT EXTEND TESTBASE
 // IT SIMPLY DOES NOT MAKE SENSE
-
 public class WebOrderUtility {
+
 
     public static void login(WebDriver driverParam){
 
@@ -31,6 +33,36 @@ public class WebOrderUtility {
         driverParam.findElement(By.id("ctl00_MainContent_password")) .sendKeys(password);
         // click login
         driverParam.findElement(By.id("ctl00_MainContent_login_button")).click();
+
+    }
+
+    public static void logout(WebDriver driverParam){
+
+        // logout link has id of ctl00_logout
+        driverParam.findElement(By.id("ctl00_logout")).click();
+
+    }
+
+    public static boolean isAtOrderPage(WebDriver driverParam){
+        // you can also check the url
+        // you can check the title if it's different
+        // whatever that makes sense
+        // in here we decided to check one element
+        boolean result = false ;
+
+        // locator for the header element of all order page
+        //h2[normalize-space(.)='List of All Orders']
+        try{
+            WebElement header = driverParam.findElement(By.xpath("//h2[normalize-space(.)='List of All Orders']"));
+            System.out.println("ELEMENT WAS IDENTIFIED ");
+            System.out.println("header.isDisplayed() = " + header.isDisplayed());
+            result = true ;
+        }catch (NoSuchElementException e){
+            System.out.println("NO Such element! you are not at the right page");
+        }
+
+        return result ;
+//        System.out.println("header.isDisplayed() = " + header.isDisplayed());
 
     }
 
